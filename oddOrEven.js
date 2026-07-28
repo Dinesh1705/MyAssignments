4279b38a51a48c07d85ce15d1202ebad223289a5
Dinesh1705 <97379664+Dinesh1705@users.noreply.github.com>
2026-07-28 16:00:18 +0530

    Create oddOrEven.js

---
+++ oddOrEven.js
@@ -0,0 +1,11 @@
+function isOddOrEven(num) {
+    if (num % 2 === 0)
+         {
+        return "Even";
+    } 
+    else
+         {
+        return "Odd";
+    }
+}
+console.log(isOddOrEven(10));
